338d9a687ab3f5c990d07667fa6e26e57a9419be
ggrangia <giacomo.grangia@gmail.com>
2024-02-08 12:13:45 +0100

    Improve rasterio reader output to support multiple bands

--- src/api/common/utils/readgeodata/rasterioreader.py
+++ src/api/common/utils/readgeodata/rasterioreader.py
@@ -1,6 +1,7 @@
+import numpy as np
 import rasterio
 import rasterio.warp
-from interfaces import GeoDataReader
+from interfaces import BandsNameNotFoundError, GeoDataReader
 from rasterio.crs import CRS
 
 
@@ -14,7 +15,6 @@ class RasterIOReader(GeoDataReader):
         with rasterio.open(filename) as ds:
             current_crs = ds.profile["crs"]
 
-            # FIXME: handle multiple coordinates
             points = [list(pair) for pair in coordinates]
             feature = {
                 "type": "MultiPoint",
@@ -26,8 +26,17 @@ class RasterIOReader(GeoDataReader):
                 CRS.from_epsg(coordinates_crs), current_crs, feature
             )
 
-            # FIXME: handle multiple bands
-            sampled_data_points = ds.sample(feature_proj["coordinates"])
-            values = [v[0] for v in sampled_data_points]
+            descriptions = ds.descriptions
+            if not all(descriptions):
+                raise BandsNameNotFoundError(f"Cannot find bands name in {filename}")
 
-        return values
+            # sampled points response is N x B where:
+            # - N is the number of coordinates
+            # - and B the number of bands
+            # we want the output to be shaped as B x N
+            sampled_data_points = ds.sample(feature_proj["coordinates"])
+            mat = np.array(list(sampled_data_points))
+            t = mat.T
+            output = dict(zip(ds.descriptions, t, strict=False))
+
+        return output
