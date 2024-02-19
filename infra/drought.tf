f4b1107d9d87c097100edad168b7bc985330a70d
ggrangia <giacomo.grangia@gmail.com>
2024-02-19 17:09:08 +0100

    Drought: create infra
    - ecr repo
    - lambda
    - github role

---
+++ infra/drought.tf
@@ -0,0 +1,82 @@
+module "drought" {
+
+  source = "./modules/github_risks_role"
+
+  role_name            = "gh_drought"
+  github_oidc_provider = var.github_oidc_provider
+  lambda_functions_arn = [module.apinine_drought.lambda_function_arn]
+  ecr_repositories     = [aws_ecr_repository.apinine_drought.arn]
+}
+
+
+resource "aws_ecr_repository" "apinine_drought" {
+  name                 = "apinine_drought"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+data "aws_iam_policy_document" "apinine_drought" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:ListAllMyBuckets",
+    ]
+    resources = ["*"]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectAttributes",
+      "s3:GetBucketLocation",
+      "s3:ListBucket",
+      "s3:GetBucketVersioning",
+      "s3:GetObjectTagging"
+    ]
+    resources = [
+      "arn:aws:s3:::mlflow-monitoring",
+      "arn:aws:s3:::mlflow-monitoring/*"
+    ]
+  }
+
+  statement {
+    effect    = "Allow"
+    actions   = ["secretsmanager:GetSecretValue"]
+    resources = [data.aws_secretsmanager_secret.apinine_gmaps_apikey.arn]
+  }
+}
+
+module "apinine_drought" {
+  source  = "terraform-aws-modules/lambda/aws"
+  version = "7.2.1"
+
+  create         = true
+  create_package = false
+
+  function_name = "apinine_drought"
+  description   = "This function returns the drought risk analysis on the provided location (address or lat and lon)."
+
+  timeout     = 30
+  memory_size = 2048
+  #ephemeral_storage_size = 1024
+
+
+  attach_tracing_policy = true
+  tracing_mode          = "Active"
+  package_type          = "Image"
+  image_uri             = "${aws_ecr_repository.apinine_drought.repository_url}:latest"
+
+  attach_policy_json = true
+  policy_json        = data.aws_iam_policy_document.apinine_drought.json
+
+  environment_variables = {
+    "POWERTOOLS_LOG_LEVEL" : "INFO",
+    "POWERTOOLS_SERVICE_NAME" : "APININE_DROUGHT",
+    "GMAPS_SECRET_NAME" : "apinine/gmaps_apikey"
+  }
+
+}
