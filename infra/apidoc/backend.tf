4368e62e3650fa5c5eeeb2e1557187a6fcf3682d
ggrangia <giacomo.grangia@gmail.com>
2024-05-20 17:23:06 +0200

    Apidoc: adding s3 resource

--- infra/apidoc/backend.tf
+++ infra/apidoc/backend.tf
@@ -5,6 +5,5 @@ terraform {
     region         = "eu-central-1"
     encrypt        = true
     dynamodb_table = "terraform-be-locktable"
-
   }
 }
--- infra/apidoc/data.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "remote" {
-  backend = "s3"
-  config = {
-    bucket = "terraform-be-bucket"
-    key    = "apinine/terraform.tfstate"
-    region = "your-aws-region"
-  }
-}
